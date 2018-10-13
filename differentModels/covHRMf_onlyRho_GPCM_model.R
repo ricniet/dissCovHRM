a0ec826af903b48f24bf665314f7614faef099fc
ricniet <nietoric@utexas.edu>
2018-10-13 11:08:27 -0500

    Added starting values function, added calculation of RHO, added calculation of etaDelta, added beta to list of parameters to follow

--- differentModels/covHRMf_onlyRho_GPCM_model.R
+++ differentModels/covHRMf_onlyRho_GPCM_model.R
@@ -2,7 +2,8 @@ library(R2jags); library(tictoc); library(tidyverse)
 
 source('./dataGeneration/genModels.R')
 
-simDat <- covHRMf.onlyRho.sim(N=500,J=8,K=5,R=40,twoPL=F,eta=c(.88,1.1))
+#load('./dataGeneration/generatedDataSets/forTests/set1_simDat_fcovHRM_onlyRHO_N500R40J8K5.Rdat')
+#simDat <- covHRMf.onlyRho.sim(N=500,J=8,K=5,R=40,twoPL=F,eta=c(.88,1.1))
 #data <- simDat$data
 data <- simDat$less_data
 subject <- data$subject
@@ -14,6 +15,9 @@ rMatrix <- simDat$raterMatrix %>% arrange(cov1,rater)
 #for constraints:
 paste('phi[',rMatrix[which(rMatrix$cov1==1),]$rater[1:(length(rMatrix[which(rMatrix$cov1==1),]$rater)-1)],']',sep='',collapse='+')
 paste('phi[',rMatrix[which(rMatrix$cov1==2),]$rater[1:(length(rMatrix[which(rMatrix$cov1==2),]$rater)-1)],']',sep='',collapse='+')
+#for saving rho
+paste('rho[',rMatrix[which(rMatrix$cov1==1),1],'] <- ','phi[',rMatrix[which(rMatrix$cov1==1),1],'] + eta[1]',collapse=' ',sep='')
+paste('rho[',rMatrix[which(rMatrix$cov1==2),1],'] <- ','phi[',rMatrix[which(rMatrix$cov1==2),1],'] + eta[2]',collapse=' ',sep='')
 # number of subjects per rater
 data %>% group_by(rater) %>% summarize(n_distinct(subject))
 
@@ -68,13 +72,17 @@ covHRM_mod = function(){
   
   # Rater and covariate effects
   
-  phi[19] <- -(phi[2] + phi[5] + phi[6] + phi[8] + phi[10] + phi[11] + phi[16] +
-                 phi[18])
+  phi[30] <- -(phi[2]+phi[5]+phi[7]+phi[8]+phi[9]+phi[10]+phi[11]+
+                 phi[15]+phi[16]+phi[17]+phi[18]+phi[22]+phi[25]+phi[28]+phi[29])
   
-  phi[20] <- -(phi[1] + phi[3] + phi[4] + phi[7] + phi[9] + phi[12] + phi[13] +
-                 phi[14] + phi[15] + phi[17])
+  phi[27] <- -(phi[1]+phi[3]+phi[4]+phi[6]+phi[12]+phi[13]+phi[14]+
+                 phi[19]+phi[20]+phi[21]+phi[23]+phi[24]+phi[26])
   
-  for(nu in 1:(R-2)){      
+  for (nu in 1:(26)){      
+    phi[nu] ~ dnorm(0, prec.phi)
+  }
+  
+  for (nu in 28:(29)){      
     phi[nu] ~ dnorm(0, prec.phi)
   }
   
@@ -110,6 +118,41 @@ covHRM_mod = function(){
   
   alpha <- 1
   
+  # save extra parameters
+  
+  etaDelta <- eta[1] - eta[2]
+  
+  rho[2] <- phi[2] + eta[1] 
+  rho[5] <- phi[5] + eta[1] 
+  rho[7] <- phi[7] + eta[1] 
+  rho[8] <- phi[8] + eta[1] 
+  rho[9] <- phi[9] + eta[1] 
+  rho[10] <- phi[10] + eta[1] 
+  rho[11] <- phi[11] + eta[1] 
+  rho[15] <- phi[15] + eta[1] 
+  rho[16] <- phi[16] + eta[1] 
+  rho[17] <- phi[17] + eta[1] 
+  rho[18] <- phi[18] + eta[1] 
+  rho[22] <- phi[22] + eta[1] 
+  rho[25] <- phi[25] + eta[1] 
+  rho[28] <- phi[28] + eta[1] 
+  rho[29] <- phi[29] + eta[1] 
+  rho[30] <- phi[30] + eta[1]
+  rho[1] <- phi[1] + eta[2] 
+  rho[3] <- phi[3] + eta[2] 
+  rho[4] <- phi[4] + eta[2] 
+  rho[6] <- phi[6] + eta[2] 
+  rho[12] <- phi[12] + eta[2] 
+  rho[13] <- phi[13] + eta[2] 
+  rho[14] <- phi[14] + eta[2] 
+  rho[19] <- phi[19] + eta[2] 
+  rho[20] <- phi[20] + eta[2] 
+  rho[21] <- phi[21] + eta[2] 
+  rho[23] <- phi[23] + eta[2] 
+  rho[24] <- phi[24] + eta[2] 
+  rho[26] <- phi[26] + eta[2] 
+  rho[27] <- phi[27] + eta[2]
+  
   # transformations
   
   #prec.alpha <- pow(sd.alpha, -2)
@@ -125,18 +168,17 @@ covHRM_data <- list("subject","rater","x","item","cov1",
 
 covHRM_inits <- function()
   list(
-    rhocov =  rnorm(R,0,.5),
-    zeta =  rep(1, each = R),
-    theta.prec = rgamma(1,100,100),
-    alpha = c(runif(J-1,.5,1.5),NA),
-    b = rnorm(J,0,.5),
-    g = matrix(rnorm(J*(K-1),0,0.25),nrow=J,ncol=K-1),
-    theta = rnorm(N, 0, 1))
+    beta = c(rnorm(J-1,0,1.2),NA),
+    kappa = c(NA,sort(rnorm(K-1,0,1.5))),
+    theta = rnorm(N, 0, 2),
+    phi = c(rnorm(26,0,1),NA,rnorm(2,0,1),NA),
+    eta = rnorm(2,0,1.2),
+    psi = runif(R,.5,1.5))
 
 # Parameters to Track and Save
 
-covHRM_params <- c("alpha","kappa","theta",
-                   "phi","eta","psi")
+covHRM_params <- c("kappa","theta","beta",
+                   "phi","eta","psi","rho","etaDelta")
 
 # Specify MCMC run
 
@@ -145,11 +187,11 @@ tic('JAGS Runtime')
 covHRM_jags <- jags.parallel(
     model.file=covHRM_mod,
     data=covHRM_data,
-    #inits=covHRM_inits,
+    inits=covHRM_inits,
     n.chains=3,
-    n.burnin=2500,
-    n.iter=5000,
-    n.thin=5,
+    n.burnin=0,
+    n.iter=3,
+    n.thin=1,
     parameters.to.save=covHRM_params,
     jags.module = 'glm', 
     jags.seed = 11071987)
@@ -157,7 +199,7 @@ toc()
 
 covHRM_results <- data.frame(round(covHRM_jags$BUGSoutput$summary,4))
 
-save(simDat, file='./testResults/fcovHRM_onlyRHO/simDat_fcovHRM_onlyRHO_N300R20J12K5_5000i2500b1t.Rdat')
-write.csv(covHRM_results, file='./testResults/fcovHRM_onlyRHO/N300R20J12K5_5000i2500b1t.csv')
+#save(simDat, file='./dataGeneration/generatedDataSets/forTests/set0_simDat_fcovHRM_onlyRHO_N500R40J8K5.Rdat')
+write.csv(covHRM_results, file='./testResults/fcovHRM_onlyRHO/N500R40J8K5_20000i10000b20t.csv')
 
-save(covHRM_jags, file='./testResults/fcovHRM_onlyRHO/N300R20J12K5_5000i2500b1t.Rdat')
+save(covHRM_jags, file='./testResults/fcovHRM_onlyRHO/covHRMf_RHOonly_test1_N500R40J8K5_20000i10000b20t.Rdat')
