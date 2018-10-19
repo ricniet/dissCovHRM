64bf8ffcbc2b0ef70940c106ac5058574b4e0868
ricniet <nietoric@utexas.edu>
2018-10-18 22:34:04 -0500

    Edits to assess function

--- justTests/assessTests.R
+++ justTests/assessTests.R
@@ -119,21 +119,94 @@ assess <- function(jagsObj, R, filePath, eval, random) {
   
   biasEtaDelta <- round(covHRM_results[which(row.names(covHRM_results)=='etaDelta'),1] - (simDat$trueValues$eta[1] - simDat$trueValues$eta[2]),3)
   
-  save(biasResults, file=paste(filePath,"biasResults.Rdat",sep=""))
-  
-  sink(file=paste(filePath,"biasResults.txt",sep=""), append=F)
-  print("Rho: Average bias (SD):")
-  sprintf("%s (%s)",aveBiasRho,sdBiasRho)
-  
-  print("Phi: Average bias (SD):")
-  sprintf("%s (%s)",aveBiasPhi,sdBiasPhi)
-  sink()
-  
   cat("RHO, Average bias (SD):\n",sprintf("%s (%s)",aveBiasRho,sdBiasRho),"\n\n",
     "PHI, Average bias (SD):\n",sprintf("%s (%s)",aveBiasPhi,sdBiasPhi),"\n\n",
     "ETA1, Bias:\n",sprintf("%s",biasEta1),"\n\n",
     "ETA2, Bias:\n",sprintf("%s",biasEta2),"\n\n",
     "etaDELTA, Bias:\n",sprintf("%s",biasEtaDelta),"\n\n",
     file=paste(filePath,"biasResults.txt",sep="")
+  )
+}
+
+assess_etaOnly <- function(jagsObj, R, filePath, eval, random) {
+  library(mcmcplots)
+  library(pryr)
+  t <- as.mcmc(jagsObj)
+  
+  # traceplot
+  
+  trace.eta %<a-% { traplot(
+    mcmc = t,
+    parms = paste('eta[',1:2,']',sep=''))}
+  
+  trace.etaDelta %<a-% { traplot(
+    mcmc = t,
+    parms = 'etaDelta')}
+  
+  if (random==T) {
+    trace.sdRho %<a-% { traplot(
+      mcmc = t,
+      parms = 'sd.rho')}
+  }
+  
+  # density
+  
+  density.eta %<a-% { denplot(
+    mcmc = t,
+    parms = paste('eta[',1:2,']',sep=''))}
+  
+  density.etaDelta %<a-% { denplot(
+    mcmc = t,
+    parms = 'etaDelta')}
+  
+  if (random==T) {
+    density.sdRho %<a-% { traplot(
+      mcmc = t,
+      parms = 'sd.rho')}
+  }
+  
+  # save traceplots
+  
+  png(paste(filePath,"resultsPlots/trace_eta.png",sep=""))
+  trace.eta
+  dev.off()
+  
+  png(paste(filePath,"resultsPlots/trace_etaDelta.png",sep=""))
+  trace.etaDelta
+  dev.off()
+  
+  if (random==T) {
+    png(paste(filePath,"resultsPlots/trace_sdRho.png",sep=""))
+    trace.sdRho
+    dev.off()
+  }
+  
+  # save density plots
+  
+  png(paste(filePath,"resultsPlots/density_eta.png",sep=""))
+  density.eta
+  dev.off()
+  
+  png(paste(filePath,"resultsPlots/density_etaDelta.png",sep=""))
+  density.etaDelta
+  dev.off()
+  
+  if (random==T) {
+    png(paste(filePath,"resultsPlots/density_sdRho.png",sep=""))
+    density.sdRho
+    dev.off()
+  }
+  
+  # calculate bias for eval
+  
+  biasEta1 <- round(covHRM_results[which(row.names(covHRM_results)=='eta[1]'),1] - simDat$trueValues$eta[1],3)
+  biasEta2 <- round(covHRM_results[which(row.names(covHRM_results)=='eta[2]'),1] - simDat$trueValues$eta[2],3)
+  
+  biasEtaDelta <- round(covHRM_results[which(row.names(covHRM_results)=='etaDelta'),1] - (simDat$trueValues$eta[1] - simDat$trueValues$eta[2]),3)
+  
+  cat("ETA1, Bias:\n",sprintf("%s",biasEta1),"\n\n",
+      "ETA2, Bias:\n",sprintf("%s",biasEta2),"\n\n",
+      "etaDELTA, Bias:\n",sprintf("%s",biasEtaDelta),"\n\n",
+      file=paste(filePath,"biasResults.txt",sep="")
   )
 }
